d9328f08f5cd110a9d2ccda87a48643bb25b86c1
eflerrr <cool.yan_sv@mail.ru>
2023-03-16 14:27:31 +0300

    allocator_1: done! (class memory and integrated logger), pre-release

--- allocator_1/allocator_1.h
+++ allocator_1/allocator_1.h
@@ -11,7 +11,7 @@ class memory
 
 private:
 
-    vector<void*> _pointers;
+    map<void*, size_t> _pointers;
     const logger* _logger;
 
 public:
@@ -24,25 +24,40 @@ public:
 
     memory &operator=(memory const &) = delete;
 
-    virtual ~memory(){
-        /////debug
-//        for(void* i : _pointers){
-//            cout << i << ' ';
-//        }
-        /////debug
-        void* p;
-        while(!_pointers.empty()){
-            p = _pointers.back();
-            //TODO----------------
-            ::operator delete(p);
-            _pointers.pop_back();
+    string memory_dump(void* const p, const size_t t){
+        auto* p2 = reinterpret_cast<unsigned char*>(p);
+        string s = "";
+        size_t dump;
+        for(int k = 0; k < t; ++k){
+            dump = (size_t)(*p2);
+            s += to_string(dump);
+            s += ' ';
+            p2++;
         }
+        return s;
     }
 
     string address_to_hex(void const * const pointer) noexcept{
         char address_buf[(sizeof(void const * const) << 1) + 3];
         sprintf(address_buf, "%#p", pointer);
         return std::string { address_buf };
+    }
+
+    virtual ~memory(){
+        /////debug
+//        for(void* i : _pointers){
+//            cout << i << ' ';
+//        }
+        /////debug
+        for(pair<void*, size_t> pointer : _pointers){
+            string msg = "Emergency deallocation of memory, address: ";
+            msg += address_to_hex(pointer.first);
+            msg += ", dump: ";
+            msg += memory_dump(pointer.first, pointer.second);
+            _logger->log(msg, logger::severity::debug);
+            ::operator delete(pointer.first);
+            _pointers.erase(pointer.first);
+        }
     }
 
     void* allocate(size_t target_size){
@@ -53,22 +68,26 @@ public:
             p = nullptr;
             _logger->log("Allocation failed, pointer with nullptr value", logger::severity::debug);
         }
-        _pointers.push_back(p);
-        string s = "Allocation complete, address: ";
-        s += address_to_hex(p);
-        _logger->log(s, logger::severity::debug);
+        _pointers[p] = target_size;
+        string msg = "Allocation complete, address: ";
+        msg += address_to_hex(p);
+        _logger->log(msg, logger::severity::debug);
         return p;
     }
 
     void deallocate(void* target_to_dealloc){
-        if(find(_pointers.begin(), _pointers.end(), target_to_dealloc) == _pointers.end()){
+        auto iter = _pointers.find(target_to_dealloc);
+        if(iter == _pointers.end()){
             _logger->log("Could not deallocate memory, unexpected pointer", logger::severity::debug);
             throw logic_error("Could not deallocate memory, unexpected pointer");
         }
-        //TODO--------------------------
-        _logger->log("Deallocation complete", logger::severity::debug);
+        string msg = "Deallocation complete, address: ";
+        msg += address_to_hex(target_to_dealloc);
+        msg += ", dump: ";
+        msg += memory_dump(target_to_dealloc, _pointers[target_to_dealloc]);
+        _logger->log(msg, logger::severity::debug);
         ::operator delete(target_to_dealloc);
-        _pointers.erase(find(_pointers.begin(), _pointers.end(), target_to_dealloc));
+        _pointers.erase(iter->first);
     }
 
 };
--- allocator_1/main.cpp
+++ allocator_1/main.cpp
@@ -7,17 +7,20 @@ int main(){
     logger* logger = builder->add_stream("console", logger::severity::debug)
                             ->build();
 
-    memory alloc(logger);
-    char* p = reinterpret_cast<char*>(alloc.allocate(12));
-    int* r = reinterpret_cast<int*>(alloc.allocate(20));
+    auto* alloc = new memory(logger);
+    char* p = reinterpret_cast<char*>(alloc->allocate(4));
+    int* r = reinterpret_cast<int*>(alloc->allocate(20));
+    *r = 1045;
     *p = 'h';
     *(p+1) = 'e';
     *(p+2) = 'y';
     *(p+3) = 0;
     string s = p;
     cout << s <<endl;
-    alloc.deallocate(reinterpret_cast<void*>(p));
+    alloc->deallocate(reinterpret_cast<void*>(p));
+//    alloc.deallocate(reinterpret_cast<void*>(r));
 
+    delete alloc;
     delete logger;
     delete builder;
 }
