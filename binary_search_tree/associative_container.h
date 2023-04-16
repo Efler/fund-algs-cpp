49fe6482fa05ca1cebd94a0de2998d910e5862fc
eflerrr <cool.yan_sv@mail.ru>
2023-04-17 02:17:52 +0300

    binary_search_tree: files created

---
+++ binary_search_tree/associative_container.h
@@ -0,0 +1,14 @@
+#ifndef BINARY_SEARCH_TREE_ASSOCIATIVE_CONTAINER_H
+#define BINARY_SEARCH_TREE_ASSOCIATIVE_CONTAINER_H
+
+template <typename tkey, typename tvalue>
+class associative_container
+{
+
+private:
+
+
+
+};
+
+#endif //BINARY_SEARCH_TREE_ASSOCIATIVE_CONTAINER_H
