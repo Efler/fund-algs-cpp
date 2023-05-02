a6f37bd36fec9edd923b7f7d0c52eee9a35ad72a
eflerrr <cool.yan_sv@mail.ru>
2023-05-02 04:43:04 +0300

    red_black_tree: inheritance of node structure and template methods (few steps)

--- red_black_tree/red_black_tree.h
+++ red_black_tree/red_black_tree.h
@@ -6,17 +6,75 @@
 template <typename tkey, typename tvalue, typename tkey_comparer>
 class red_black_tree final : public binary_search_tree<tkey, tvalue, tkey_comparer>
 {
+private:
 
+    enum class color{
+        red,
+        black
+    };
 
+//    struct rb_node final : public binary_search_tree<tkey, tvalue, tkey_comparer>::node{
+    struct node final : public binary_search_tree<tkey, tvalue, tkey_comparer>::node{     //////TODO ???
+        node() = default;
+        node(tkey k, tvalue v, node* l, node* r, color c):
+                key(k), value(v), left(l), right(r), node_color(c){}
 
+        tkey key;
+        tvalue value;
+        node* left;
+        node* right;
+        color node_color;
+
+        virtual ~node() = default;
+    };
+
+private:
+
+    class rb_find_template_method final : public binary_search_tree<tkey, tvalue, tkey_comparer>::find_template_method
+    {
+    public:
+
+        void after_find(typename associative_container<tkey, tvalue>::key_value_pair* target_key_and_result_value, node* root) override {
+
+        }
+
+    };
+
+    class rb_insert_template_method final : public binary_search_tree<tkey, tvalue, tkey_comparer>::insert_template_method
+    {
+    private:
+
+        stack<node*> _insert_path = stack<node*>();
+
+    public:
+
+        void after_insert(const tkey &key, const tvalue &value, node** root, logger* logger) override {
+
+        }
+
+    };
+
+    class rb_remove_template_method final : public binary_search_tree<tkey, tvalue, tkey_comparer>::remove_template_method
+    {
+    private:
+
+        stack<node*> _remove_path = stack<node*>();
+
+    public:
+
+        void after_remove(const tkey &key, node** root, logger* logger) override {
+
+        }
+
+    };
 
 private:
 
     logger* _logger;
     abstract_allocator* _allocator;
-    typename binary_search_tree<tkey, tvalue, tkey_comparer>::find_template_method* _find;
-    typename binary_search_tree<tkey, tvalue, tkey_comparer>::insert_template_method* _insert;
-    typename binary_search_tree<tkey, tvalue, tkey_comparer>::remove_template_method* _remove;
+    rb_find_template_method* _find;
+    rb_insert_template_method* _insert;
+    rb_insert_template_method* _remove;
 
 public:
 
